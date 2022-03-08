da73b5e24277e3c763035562d9ce8226787335db
Vladimir Besser <vladimirbesser@gmail.com>
2022-03-08 00:53:13 -0300

    Manejar menus

---
+++ Menu/Api.h
@@ -0,0 +1,11 @@
+#ifndef API_H
+#define API_H
+
+#define ID_Agregar 100
+#define ID_Eliminar 101
+#define ID_Salir 102
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam,LPARAM lParam);
+
+
+#endif
